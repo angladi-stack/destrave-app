72cbac41195807c18f1fea003b1c8c5601e9452d
angladi-stack <angladi.dlo@gmail.com>
2026-09-16 00:46:48 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { DestraveApp } from './DestraveApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <DestraveApp />
+  </React.StrictMode>
+);
