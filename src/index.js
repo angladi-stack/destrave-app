87afdf4f2145e1dca5e4fa795f2f9b28c3a0bbda
angladi-stack <angladi.dlo@gmail.com>
2026-09-16 01:27:31 -0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { DestraveApp } from './DestraveApp';
+import DestraveApp from './DestraveApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
